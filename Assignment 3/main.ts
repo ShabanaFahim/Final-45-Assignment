7de9190bac8ff0eb66e7e40a2f1e1d24a1b8b2f3
“[shabana <[libra786foru@hotmail.com]>
2024-03-02 01:30:11 +0500

    first commit

---
+++ Assignment 3/main.ts
@@ -0,0 +1,9 @@
+// storing the famous quote and its auther
+
+let quote:string = "All our dreams can come true, if we have the courage to persue them"
+
+let auther:string="Walt Desney"
+
+//printing the message
+
+console.log(`"${quote}"-${auther}`);
